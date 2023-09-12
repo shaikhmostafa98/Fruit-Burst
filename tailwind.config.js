8c41735678e9b9e7032953be94934bd00bff47fe
shaikhMostafa <shaikhmostafa21@gmail.com>
2023-09-12 06:14:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
